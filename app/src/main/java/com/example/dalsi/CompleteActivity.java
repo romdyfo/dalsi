ea1dd4102983b8eafdfa833483578b6b90fecae0
romdy <kimjh0937@gmail.com>
2023-08-08 23:35:24 +0900

    문의하기 화면전환 기능 추가

---
+++ app/src/main/java/com/example/dalsi/CompleteActivity.java
@@ -0,0 +1,14 @@
+package com.example.dalsi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CompleteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete);
+    }
+}
